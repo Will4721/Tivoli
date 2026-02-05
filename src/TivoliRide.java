8ecf77a587c69f9dc4f520369c4d2970f366fd6a
Will4721 <williamlehd@gmail.com>
2026-02-05 11:02:13 +0100

    more req

--- src/TivoliRide.java
+++ src/TivoliRide.java
@@ -4,11 +4,14 @@ public class TivoliRide {
     int height;
 
 void work(){
-    if (height < 160 ){
+    if (height < 160){
         System.out.println("your not allowed to ride gyldne tårn");
     }
-    if (height >= 160){
+
+    if (height >= 160 && height <= 180){
         System.out.println("You may try the ride: gyldne tårn");
+    }else{
+        System.out.println("your not allowed to ride gyldne tårn");
     }
 }
     }
