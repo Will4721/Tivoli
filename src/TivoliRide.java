a2af1119657cfa87fbaa569a0b16ff082c518342
Will4721 <williamlehd@gmail.com>
2026-02-05 09:49:26 +0100

    first push

--- src/TivoliRide.java
+++ src/TivoliRide.java
@@ -1,13 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class TivoliRide {
+    public static void main(String[] args) {
+        System.out.println("Please enter name");
+        Scanner newObj = new Scanner(System.in);
+        String resuname = newObj.nextLine();
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Please enter height");
+        int resu = myObj.nextInt();
+        if (resu > 170){
+            System.out.println("Your tall enough too try all the rides. You can choose between\nA: carousel\nB: bumber carts\nC: The rollercoaster\nType 'A' or 'B' or 'C'");
+            String resu4 = newObj.nextLine();
+            if (resu4.equals("A")||resu4.equals("a")){
+                System.out.println("Your now on the carousel have fun :)");
+            }
+            if (resu4.equals("B")||resu4.equals("b")){
+                System.out.println("Your now on the bumber carts have fun :)");
+            }
+            if (resu4.equals("C")||resu4.equals("c")){
+                System.out.println("Your now on the rollercoaster have fun :)");
+            }
+        }
+        if (resu < 170){
+            System.out.println("sorry " + resuname + " your not allowed to try the ultimate rollercoaster that is super fun and everybody loves, too bad");
+            System.out.println("May i suggest the carousel or perhaps the bumber carts instead\nA: carousel\nB: bumber carts\nType 'A' or 'B'");
+            String resu4 = newObj.nextLine();
+            if (resu4.equals("A")||resu4.equals("a")){
+                System.out.println("Your now on the carousel have fun :)");
+            }
+            if (resu4.equals("B")||resu4.equals("b")){
+                System.out.println("Your now on the bumber carts have fun :)");
+            }
+
+        }
     }
 }
